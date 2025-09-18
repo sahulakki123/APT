aafb1cc44f22e1a776ce5e06ee69458a2ba02dcf
sahulakki123 <lakkisahus04@gmail.com>
2025-09-18 18:29:21 +0530

    API

--- project/settings.py
+++ project/settings.py
@@ -115,9 +115,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+import os
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# This is required for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# For development (optional but useful)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
